a6239ddb70e0c25b02f7d8f23990a86e7b0480ac
CoralineAda <coraline@idolhands.com>
2015-01-15 23:20:16 -0600

    Suppress and handle errors from ::parser

--- lib/module_mapper/map.rb
+++ lib/module_mapper/map.rb
@@ -2,17 +2,31 @@ module ModuleMapper
   module Map
 
     def self.from(content:, filename:)
-      parser = Analyst.for_source(content)
-
-      containers = parser.top_level_entities.select{|e| e.respond_to? :all_methods}
-      containers << containers.map(&:classes)
-      containers.flatten!.reject!{ |container| container.all_methods.empty? }
-
-      containers.map do |container|
-        {
-          module_name: container.full_name,
-          filename: filename
-        }
+      orig_stderr = $stderr.clone
+      orig_stdout = $stdout.clone
+      $stderr.reopen File.new('/dev/null', 'w')
+      $stdout.reopen File.new('/dev/null', 'w')
+      begin
+        parser = Analyst.for_source(content)
+        containers = parser.top_level_entities.select{|e| e.respond_to? :all_methods}
+        containers << containers.map(&:classes)
+        containers.flatten!.reject!{ |container| container.all_methods.empty? }
+        containers.map do |container|
+          {
+            module_name: container.full_name,
+            filename: filename
+          }
+        end
+      rescue
+        containers = [
+          {
+            module_name: "Unknown",
+            filename: filename
+          }
+        ]
+      ensure
+        $stdout.reopen orig_stdout
+        $stderr.reopen orig_stderr
       end
     end
 
--- lib/module_mapper/parser.rb
+++ lib/module_mapper/parser.rb
@@ -9,24 +9,21 @@ module ModuleMapper
     end
 
     def report
-      module_map.each do |mmap|
-        puts "#{mmap[:filename]}\t#{mmap[:module_name]}"
+      file_reader.source_files.each do |source_file|
+        maps(source_file).each do |mmap|
+          puts "#{mmap[:filename]}\t#{mmap[:module_name]}"
+        end
       end
     end
 
     private
 
-    def file_reader
-      @file_reader ||= ModuleMapper::FileReader.new(self.paths_to_files)
+    def maps(source_file)
+      ModuleMapper::Map.from(filename: source_file.filename, content: source_file.contents)
     end
 
-    def module_map
-      @map ||= file_reader.source_files.map do |source_file|
-        ModuleMapper::Map.from(
-          filename: source_file.filename,
-          content: source_file.contents
-        )
-      end.flatten
+    def file_reader
+      @file_reader ||= ModuleMapper::FileReader.new(self.paths_to_files)
     end
 
   end
